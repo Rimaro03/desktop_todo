067b2253ccd720643aa84c0394b0bae09415edbf
Rimaro <leonardo.ongaro2003@hotmail.com>
2022-03-17 22:20:02 +0100

    added task list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
-import React from 'react';
-import CalendarPicker from '@mui/lab/CalendarPicker';
-import LocalizationProvider from '@mui/lab/LocalizationProvider';
-import AdapterDateFns from '@mui/lab/AdapterDateFns';
+import Calendar from "./components/calendar";
+import Tasklist from "./components/tasklist";
+import "./style.css"
 
 export default function App() {
-  const [selectedDate, setSelectedDate] = React.useState(new Date());
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
-
   return (
-    <LocalizationProvider dateAdapter={AdapterDateFns}>
-      <CalendarPicker
-        date={selectedDate}
-        onChange={handleDateChange}
-        shouldDisableDate={(date) => date.getDay() === 0 || date.getDay() === 6}
-      />
-    </LocalizationProvider>
-  );
+    <div className="main">
+      <div className="calendar">
+        <Calendar />
+      </div>
+      <div className="taskListDiv">
+        <Tasklist />
+      </div>
+      <div className="taskListDiv">
+        <Tasklist />
+      </div>
+    </div>
+  )
 }
