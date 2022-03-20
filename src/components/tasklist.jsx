bfa2cf3ef7895acccfcb772676585e6b54cba1db
Rimaro <leonardo.ongaro2003@hotmail.com>
2022-03-20 18:35:27 +0100

    fixed state bug

--- src/components/tasklist.jsx
+++ src/components/tasklist.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "../style.css";
 
 export default function Tasklist() {
@@ -6,34 +6,44 @@ export default function Tasklist() {
   const [taskList, setTaskList] = useState([]);
   const [taskListDone, setTaskListDone] = useState([]);
 
+  useEffect(() => console.log(taskList), [taskList]);
+
   const handleItem = (index) => {
     const newTaskList = [...taskList];
     newTaskList.splice(index, 1);
     setTaskList(newTaskList);
     setTaskListDone([...taskListDone, taskList[index]]);
-  }
+  };
 
   const addItem = () => {
     console.log(taskItem);
-    taskList.push(taskItem);
-    setTaskList([...taskList, "ciao"]);
+    setTaskList([...taskList, taskItem]);
+    setTaskItem("");
     console.log(taskList);
-  }
+  };
 
   return (
     <div className="lists">
       <div className="list taskList">
-        {
-          taskList.map((item, index) => {
-            return(
-              <p className="taskItem" onClick={handleItem(index)} key={index}>{item}</p>
-            )
-          })
-        }
+        {taskList.map((item, index) => {
+          return (
+            <p
+              className="taskItem"
+              onClick={() => handleItem(index)}
+              key={index}
+            >
+              {item}
+            </p>
+          );
+        })}
       </div>
-      <div className="list completedList">
-      </div>
-      <input type="text" name="" id="" onKeyDown={e => setTaskItem(taskItem+e.key)}/>
+      <div className="list completedList"></div>
+      <input
+        type="text"
+        name=""
+        id=""
+        onKeyDown={(e) => setTaskItem(taskItem + e.key)}
+      />
       <button onClick={() => addItem()}></button>
     </div>
   );
